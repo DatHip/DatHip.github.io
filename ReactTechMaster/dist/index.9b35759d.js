dcaa0daa564f86d4d4b8d9d5e768fb2ec56daae7
Dat Hip <hb1999vn@gmail.com>
2022-06-03 12:06:12 +0700

    asdas
    asd

---
+++ ReactTechMaster/dist/index.9b35759d.js
@@ -0,0 +1,605 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function TrItems(props) {
+    return /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 5,
+            columnNumber: 7
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 6,
+            columnNumber: 10
+        },
+        __self: this
+    }, props.id), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 7,
+            columnNumber: 10
+        },
+        __self: this
+    }, props.name), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 8,
+            columnNumber: 10
+        },
+        __self: this
+    }, props.price), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 9,
+            columnNumber: 10
+        },
+        __self: this
+    }, props.rank), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 10,
+            columnNumber: 10
+        },
+        __self: this
+    }, props.comment));
+}
+function Label() {
+    return /*#__PURE__*/ React.createElement("table", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 16,
+            columnNumber: 7
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("caption", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 17,
+            columnNumber: 10
+        },
+        __self: this
+    }, "H\xf3a \u0111\u01A1n \u0103n nh\u1EADu"), /*#__PURE__*/ React.createElement("thead", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 19,
+            columnNumber: 10
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 20,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("th", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 21,
+            columnNumber: 16
+        },
+        __self: this
+    }, "STT"), /*#__PURE__*/ React.createElement("th", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 22,
+            columnNumber: 16
+        },
+        __self: this
+    }, "T\xean"), /*#__PURE__*/ React.createElement("th", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 23,
+            columnNumber: 16
+        },
+        __self: this
+    }, "M\u1EE9c \u0111\xf3ng g\xf3p"), /*#__PURE__*/ React.createElement("th", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 24,
+            columnNumber: 16
+        },
+        __self: this
+    }, "X\u1EBFp h\u1EA1ng"), /*#__PURE__*/ React.createElement("th", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 25,
+            columnNumber: 16
+        },
+        __self: this
+    }, "Nh\u1EADn x\xe9t"))), /*#__PURE__*/ React.createElement("tbody", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 29,
+            columnNumber: 10
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement(TrItems, {
+        id: 1,
+        name: "Th\u1EA7y Ba",
+        price: 1000000,
+        rank: "Kim C\u01B0\u01A1ng",
+        comment: "Sugar Daddy",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 30,
+            columnNumber: 13
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement(TrItems, {
+        id: 2,
+        name: "Nguy\u1EC5n Th\u1EBF Anh",
+        price: 500000,
+        rank: "B\u1EA1c",
+        comment: "Sugar Baby",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 31,
+            columnNumber: 13
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement(TrItems, {
+        id: 3,
+        name: "B\xf9i Lan Nhi",
+        price: 0,
+        rank: "Nh\xf4m",
+        comment: "\u0110\u1ECBnh m\u1EDDi Nhi \u0103n t\u1ED1i h\xf3a ra l\u1EA1i th\xe0nh b\u1EEFa t\u1ED1i c\u1EE7a Nhi",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 32,
+            columnNumber: 13
+        },
+        __self: this
+    })), /*#__PURE__*/ React.createElement("tfoot", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 41,
+            columnNumber: 10
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 42,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        colSpan: "5",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 43,
+            columnNumber: 16
+        },
+        __self: this
+    }, "T\u1ED5ng bill ca 1: 2022000")), /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 45,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        colSpan: "5",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 46,
+            columnNumber: 16
+        },
+        __self: this
+    }, "T\u1ED5ng bill ca 2: 105000")), /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 48,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        colSpan: "5",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 49,
+            columnNumber: 16
+        },
+        __self: this
+    }, "T\u1ED5ng \u0111\xf3ng g\xf3p: ", /*#__PURE__*/ React.createElement("span", {
+        className: "total",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 50,
+            columnNumber: 34
+        },
+        __self: this
+    })))));
+}
+function Label2() {
+    return /*#__PURE__*/ React.createElement("table", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 60,
+            columnNumber: 7
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("caption", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 61,
+            columnNumber: 10
+        },
+        __self: this
+    }, "B\u1EA3ng ch\xfa th\xedch"), /*#__PURE__*/ React.createElement("thead", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 63,
+            columnNumber: 10
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 64,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("th", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 65,
+            columnNumber: 16
+        },
+        __self: this
+    }, "M\u1EE9c \u0111\xf3ng g\xf3p"), /*#__PURE__*/ React.createElement("th", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 66,
+            columnNumber: 16
+        },
+        __self: this
+    }, "X\u1EBFp h\u1EA1ng"))), /*#__PURE__*/ React.createElement("tbody", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 70,
+            columnNumber: 10
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 71,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 72,
+            columnNumber: 16
+        },
+        __self: this
+    }, "Tr\xean 1000000"), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 73,
+            columnNumber: 16
+        },
+        __self: this
+    }, "Kim C\u01B0\u01A1ng")), /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 75,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 76,
+            columnNumber: 16
+        },
+        __self: this
+    }, "800000 - 1000000"), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 77,
+            columnNumber: 16
+        },
+        __self: this
+    }, "B\u1EA1ch kim")), /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 79,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 80,
+            columnNumber: 16
+        },
+        __self: this
+    }, "500000 - 800000"), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 81,
+            columnNumber: 16
+        },
+        __self: this
+    }, "V\xe0ng")), /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 83,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 84,
+            columnNumber: 16
+        },
+        __self: this
+    }, "200000 - 500000"), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 85,
+            columnNumber: 16
+        },
+        __self: this
+    }, "B\u1EA1c")), /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 87,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 88,
+            columnNumber: 16
+        },
+        __self: this
+    }, "D\u01B0\u1EDBi 200000"), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 89,
+            columnNumber: 16
+        },
+        __self: this
+    }, "\u0110\u1ED3ng")), /*#__PURE__*/ React.createElement("tr", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 91,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 92,
+            columnNumber: 16
+        },
+        __self: this
+    }, "0"), /*#__PURE__*/ React.createElement("td", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 93,
+            columnNumber: 16
+        },
+        __self: this
+    }, "Nh\xf4m"))));
+}
+// Clock
+function ClockAll() {
+    setTimeout(()=>{
+        const UI = {
+            date: document.querySelector(".date"),
+            am_pm: document.querySelector(".am-pm"),
+            second: document.querySelector(".hand--second"),
+            minute: document.querySelector(".hand--minute"),
+            hour: document.querySelector(".hand--hour")
+        };
+        function updateClock() {
+            const now = new Date();
+            const date = now.getDate();
+            const seconds = (now.getSeconds() + now.getMilliseconds() / 1000) / 60 * 360;
+            const minute = (now.getMinutes() + now.getSeconds() / 60) / 60 * 360;
+            const hours = (now.getHours() + now.getMinutes() / 60) / 12 * 360;
+            // Ui update
+            UI.date.innerHTML = date;
+            UI.second.style.transform = `rotate(${seconds}deg)`;
+            UI.minute.style.transform = `rotate(${minute}deg)`;
+            UI.hour.style.transform = `rotate(${hours}deg)`;
+            requestAnimationFrame(updateClock);
+        }
+        requestAnimationFrame(updateClock);
+    });
+    return /*#__PURE__*/ React.createElement("div", {
+        className: "container",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 130,
+            columnNumber: 7
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("div", {
+        className: "clock--wrapper",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 131,
+            columnNumber: 10
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("h2", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 132,
+            columnNumber: 13
+        },
+        __self: this
+    }, "My Time "), /*#__PURE__*/ React.createElement("div", {
+        className: "clock",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 133,
+            columnNumber: 13
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("svg", {
+        className: "clockface",
+        width: 300,
+        height: 300,
+        viewBox: "-150 -150 300 300",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 134,
+            columnNumber: 16
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("circle", {
+        className: "ring ring--seconds",
+        r: 145,
+        pathLength: 60,
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 135,
+            columnNumber: 19
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement("circle", {
+        className: "ring ring--hours",
+        r: 145,
+        pathLength: 12,
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 136,
+            columnNumber: 19
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement("text", {
+        x: 50,
+        y: -5,
+        className: "date",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 137,
+            columnNumber: 19
+        },
+        __self: this
+    }, "23"), /*#__PURE__*/ React.createElement("text", {
+        x: 50,
+        y: 10,
+        className: "am-pm",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 140,
+            columnNumber: 19
+        },
+        __self: this
+    }, "am"), /*#__PURE__*/ React.createElement("line", {
+        className: "hand hand--minute",
+        x1: 0,
+        y1: 2,
+        x2: 0,
+        y2: -110,
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 143,
+            columnNumber: 19
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement("line", {
+        className: "hand hand--hour",
+        x1: 0,
+        y1: 2,
+        x2: 0,
+        y2: -60,
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 144,
+            columnNumber: 19
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement("circle", {
+        className: "ring ring--center",
+        r: 3,
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 145,
+            columnNumber: 19
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement("line", {
+        className: "hand hand--second",
+        x1: 0,
+        y1: 12,
+        x2: 0,
+        y2: -130,
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 146,
+            columnNumber: 19
+        },
+        __self: this
+    }))), /*#__PURE__*/ React.createElement("select", {
+        name: "",
+        id: "",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 149,
+            columnNumber: 13
+        },
+        __self: this
+    })));
+}
+function All() {
+    return /*#__PURE__*/ React.createElement("div", {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 157,
+            columnNumber: 7
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement("div", {
+        className: "wrapper",
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 158,
+            columnNumber: 10
+        },
+        __self: this
+    }, /*#__PURE__*/ React.createElement(Label, {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 159,
+            columnNumber: 13
+        },
+        __self: this
+    }), /*#__PURE__*/ React.createElement(Label2, {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 160,
+            columnNumber: 13
+        },
+        __self: this
+    })), /*#__PURE__*/ React.createElement(ClockAll, {
+        __source: {
+            fileName: "app.jsx",
+            lineNumber: 162,
+            columnNumber: 10
+        },
+        __self: this
+    }));
+}
+root.render(/*#__PURE__*/ React.createElement(All, {
+    __source: {
+        fileName: "app.jsx",
+        lineNumber: 167,
+        columnNumber: 13
+    },
+    __self: this
+}));
+
+//# sourceMappingURL=index.9b35759d.js.map
